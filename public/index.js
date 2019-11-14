5aac6d3b54b0e5a022ded411514752c3a8472881
zjutzw <zjutzw@Hotmail.com>
2019-11-14 02:49:44 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -38,7 +38,4 @@ console.log('nameadded');
 }
 var eleAddname=document.createElement('div');
 document.getElementById("content").appendChild(eleAddname);
-eleAddname.innerHTML='
-	<div id="namelist"></div>
-	<input type="text" name="name" id="inputname" />
-<button onclick="addname()">addname</button>';
+eleAddname.innerHTML='<div id="namelist"></div><input type="text" name="name" id="inputname" /><button onclick="addname()">addname</button>';
